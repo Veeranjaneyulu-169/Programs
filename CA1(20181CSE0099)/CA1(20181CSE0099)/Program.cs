4c91bc28df8e672db8a645e1986ce18d3923c843
Veeranjaneyulu-169 <69708026+Veeranjaneyulu-169@users.noreply.github.com>
2020-12-23 13:05:38 +0530

    Add files via upload

---
+++ CA1(20181CSE0099)/CA1(20181CSE0099)/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1_20181CSE0099_
+{
+    public abstract class Vehicle
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+        public string Make
+        {
+            get 
+            { 
+                return make;
+            }
+            set 
+            { 
+                make = value;
+            }
+        }
+        public string Model
+       
+        {
+            get 
+            { 
+                return model; 
+            }
+            set 
+            {
+                model = value; 
+            }
+        }
+        public int Year
+        {
+            get 
+            { 
+                return year; 
+            }
+            set 
+            { 
+                year = value; 
+            }
+        }
+        public double Price
+        {
+            get 
+            { 
+                return price;
+            }
+            set 
+            { 
+                price = value;
+            }
+        }
+
+        public abstract void checkPrice();
+        public abstract void printInfo();
+        public abstract void testDrive();
+    }
+    class Minivan : Vehicle
+    {
+        public Minivan(string make, string model, int year, double price)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+        public override void checkPrice()
+        {
+            Console.WriteLine(Price);
+        }
+        public override void printInfo()
+        {
+            Console.WriteLine(Make + Model + Year + Price);
+        }
+        public override void testDrive()
+        {
+            Console.WriteLine("testDrive");
+        }
+    }
+    class SchoolBus : Vehicle
+    {
+        public SchoolBus(string make, string model, int year, double price)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+        public override void checkPrice()
+        {
+            Console.WriteLine(Price);
+        }
+        public override void printInfo()
+        {
+            Console.WriteLine(Make + Model + Year + Price);
+        }
+        public override void testDrive()
+        {
+            Console.WriteLine("testDrive");
+        }
+    }
+}
